e9ede7a461ec54f4671e4e14b3f61702ce66015d
QS Apps <app@quickscraper.co>
2024-01-31 12:25:38 +0530

    Remove access token

--- facebook.php
+++ facebook.php
@@ -1,8 +1,9 @@
 <?php
 require_once './vendor/autoload.php';
 $quickScraperClient = new QuickScraper\Main\QuickScraperClass();
-$quickScraperClient->setAccessToken('SoAH1r0gNFt1BpdmCO6oVygsp');
+$quickScraperClient->setAccessToken('YOUR_ACCESS_TOKEN');
 // $response = $quickScraperClient->getHtml('https://www.facebook.com/TSA-55782529079'); // wrong url
 $response = $quickScraperClient->getHtml('https://www.facebook.com/people/TSA/100063654012735'); // correct url
 print_r(json_decode($response));
+
 ?>
